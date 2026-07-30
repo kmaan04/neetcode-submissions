8ae4fade24cfd964d71bc56ffd26ba4041fca857
Amaan Aab Khan <aaab770@aucklanduni.ac.nz>
2026-07-30 18:50:23 +1200

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0,  1
+        res = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                sold = prices[r] - prices[l]
+                res = max(res, sold)
+            else:
+                l = r
+            r += 1
+        
+        return res
+
+
+        
